203e404d197c138bb3fe3dfa8f195db419cd29e3
saki1 <saki1@DESKTOP-JKA3VVP>
2019-04-04 23:55:45 +0900

    회원정보 수정 부분 작성 완료

---
+++ semiProjectGroup1/src/controller/User_MyPageController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.User_UserInfoDao;
+import vo.User_UserInfoVo;
+@WebServlet("/user/detail.do")
+public class User_MyPageController extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String userId=req.getParameter("userId");
+		System.out.println("userId"+userId);
+		User_UserInfoDao dao = new User_UserInfoDao();
+		User_UserInfoVo vo = dao.detail(userId);
+		System.out.println("vo"+vo);
+		req.setAttribute("vo", vo);
+		req.getRequestDispatcher("/main/layout.jsp?spage=/user/user_myPage.jsp").forward(req, resp);
+	}
+}
---
+++ semiProjectGroup1/src/controller/User_UpdateController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.User_UserInfoDao;
+import vo.User_UserInfoVo;
+
+
+@WebServlet("/update.do")
+public class User_UpdateController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		String userId=req.getParameter("userId");
+		User_UserInfoDao dao=new User_UserInfoDao();
+		User_UserInfoVo vo=dao.detail(userId);
+		req.setAttribute("vo", vo);
+		req.getRequestDispatcher("/user/user_update.jsp").forward(req, resp);
+	}
+		@Override
+		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			req.setCharacterEncoding("utf-8");
+			String userId = req.getParameter("userId");
+			String userPwd = req.getParameter("userPwd");
+			String userNickName = req.getParameter("userNickName");
+			User_UserInfoVo vo = new User_UserInfoVo(userId, userPwd, userNickName, 0, 0, null, 0);
+			User_UserInfoDao dao = new User_UserInfoDao();
+			int n=dao.update(vo);
+			if(n>0) {
+				req.setAttribute("code", "success");
+			}else {
+				req.setAttribute("code", "fail");
+			}
+			req.getRequestDispatcher("/user/user_result.jsp").forward(req, resp);
+	}
+}
