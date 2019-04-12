de4da63445432da42f28a381fca2930cf5e0dcdb
saki1 <saki1@DESKTOP-JKA3VVP>
2019-04-12 15:32:02 +0900

    dz

--- semiProjectGroup1/WebContent/ETC/평점관련 sql.sql
+++ semiProjectGroup1/WebContent/ETC/평점관련 sql.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW good
       AS 
-select u.userId userId, m.movienum movienum, m.movieReleaseDate movieReleaseDate,r.rate rate, g.genreNum genreNum
-from userinfo u, movieinfo m, ratetable r, genre g, moviegenre mg
+select u.userId userId, m.movienum movienum, m.movieReleaseDate movieReleaseDate,g.genreNum genreNum
+from userinfo u, movieinfo m, genre g, moviegenre mg, ratetable r
 where u.userid = r.userid and m.movienum=r.movienum and m.movienum=mg.movienum and g.genrenum=mg.genrenum;
